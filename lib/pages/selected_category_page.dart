bad54a7678962396b37f20b3ccfe3c337cbb4371
unknown <you@example.com>
2021-10-09 20:42:17 +0600

    category list page UI done

---
+++ lib/pages/selected_category_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectedCategoryPage extends StatelessWidget {
+  const SelectedCategoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
